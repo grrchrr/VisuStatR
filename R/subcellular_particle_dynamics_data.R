556a33abb4f90be19c6ef3ed71d3ebb19116f0ea
grrchrr <39835337+grrchrr@users.noreply.github.com>
2022-02-28 19:15:03 +0100

    minor changes pkg site

--- R/subcellular_particle_dynamics_data.R
+++ R/subcellular_particle_dynamics_data.R
@@ -1,7 +1,7 @@
 #' Subcellular particle dynamics data of labeled genomic loci in nuclei of genome engineered cell lines
 #'
-#' @description This dataframe contains tracking data based on the imaging data: \code{\link{subcellular_particle_dynamcis_images}}.
-#' Spots for the 15 first frames of the original dataset were identified and tracked with the FiJi plugin TrackMate.
+#' @description This dataframe contains tracking data based on the imaging data: \code{\link{subcellular_particle_dynamics_images}}.
+#' Spots for the 15 first frames of the original dataset were identified and tracked with the Fiji plugin TrackMate.
 #'
 #' @name subcellular_particle_dynamics
 #' @docType data
